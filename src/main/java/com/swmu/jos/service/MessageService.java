7dcdd4d2b41f8b40e92033ac2a9e14011a12859e
Kevin Durant <kevin.durant@sweetmustard.be>
2023-10-13 13:44:40 +0200

    qdezfdsdf

--- src/main/java/com/swmu/jos/service/MessageService.java
+++ src/main/java/com/swmu/jos/service/MessageService.java
@@ -1,67 +1,31 @@
 package com.swmu.jos.service;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.swmu.jos.model.Message;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
 
+    private final FirebaseApp app;
+
     private final List<Message> messages = new ArrayList<>();
+
+    public MessageService(final FirebaseApp app) {
+        this.app = app;
+    }
 
     public Collection<Message> getAll() throws IOException {
         return messages.stream().toList();
     }
 
     public void save(final Message message) throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/service-account.json");
 
-// Initialize the app with a service account, granting admin privileges
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                // The database URL depends on the location of the database
-                .setDatabaseUrl("https://joss-d2476-default-rtdb.europe-west1.firebasedatabase.app/")
-                .build();
-        FirebaseApp.initializeApp(options);
-
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-        DatabaseReference ref = FirebaseDatabase.getInstance()
-                .getReference("restricted_access/secret_document");
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Object document = dataSnapshot.getValue();
-                System.out.println(document);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-            }
-        });
-        DatabaseReference messages = ref.child("messages");
-        Map<String, Message> messageMap = new HashMap<>();
-        messageMap.put(UUID.randomUUID().toString(), message);
-        messages.setValueAsync(messageMap);
     }
 }
